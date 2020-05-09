c65f40294a2f6be9d7f9eeb5be9aa5a3032bc3cb
Lucky Fox Design <sovenkovmaksim@gmail.com>
2020-05-10 02:22:57 +0300

    Removed ym from app

--- src/App.svelte
+++ src/App.svelte
@@ -114,24 +114,6 @@
 	}
 </script>
 
-<svelte:head>
-	<!-- Yandex.Metrika counter -->
-<script type="text/javascript" >
-	(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-	m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-	(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-	ym(61801633, "init", {
-			 clickmap:true,
-			 trackLinks:true,
-			 accurateTrackBounce:true,
-			 webvisor:true
-	});
-</script>
-<noscript><div><img src="https://mc.yandex.ru/watch/61801633" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->
-</svelte:head>
-
 <main>
 	<div class="app app_size">
 		<div class="app__container">
