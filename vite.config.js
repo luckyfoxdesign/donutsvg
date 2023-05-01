bafe06be9afd3b19f1822b68abf9bad1760926ad
luckyfoxdesign <luckyfoxdesign@yandex.ru>
2023-05-01 16:32:36 +0200

    added docker compose file, npm dependencies were updated

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  server: {
+    host: true,
+    port: 3000
+}
 })
